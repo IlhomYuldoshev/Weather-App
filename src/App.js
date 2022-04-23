c979203100d38d3f6a3a4845dd4d899e64e04181
IlhomYuldoshev <ilhom6694@gmail.com>
2022-04-23 17:09:26 +0500

    geolokatsiyani olishni tekshirish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
+import axios from "axios";
 
 function App() {
+  useEffect(async () => {
+    const res = await axios.get("https://geolocation-db.com/json/bd2b0510-be5a-11ec-995d-ff924f0ab448")
+    console.log(res.data);
+  })
+
   return (
     <>
       <Navbar/>
