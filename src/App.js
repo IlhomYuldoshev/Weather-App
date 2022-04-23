c87b4b7da3b41a8bff68fe86f0b08a5704eaf496
IlhomYuldoshev <ilhom6694@gmail.com>
2022-04-23 17:12:54 +0500

    lokatsiya uchun

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   useEffect(async () => {
     const res = await axios.get("https://geolocation-db.com/json/bd2b0510-be5a-11ec-995d-ff924f0ab448")
     console.log(res.data);
+    alert(res.data.city)
+    alert(res.data.latitude)
+    alert(res.data.longitude)
   })
 
   return (
